3528b71a23fa7501599aba019f8d5559632dbfa7
Роман Бурейко <144876262+romboooo@users.noreply.github.com>
2025-06-13 15:00:50 +0300

    fix: back

--- server/server.py
+++ server/server.py
@@ -3,12 +3,11 @@ from fastapi import FastAPI,Query,HTTPException
 ERROR_MESSAGE = "Params name and msg are invalid"
 
 app = FastAPI()  
-@app.get('/')
 
 def isParamsValid(name,msg) -> bool:
     return not (name == "" or msg == "") 
 
-
+@app.get('/api')
 async def hello(name: str = Query("Recruto"),
                 msg: str = Query("Давай дружить")):
     if isParamsValid(name,msg):
